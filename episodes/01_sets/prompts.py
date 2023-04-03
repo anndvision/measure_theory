76ba97df4af48132884baf68e8b811568149008f
Andrew Jesson <56548831+anndvision@users.noreply.github.com>
2023-04-03 08:53:05 +0000

    add episode 1

---
+++ episodes/01_sets/prompts.py
@@ -0,0 +1,14 @@
+prompts = {
+    "0": "book cover for 'Measure Theory Part 1, Sets' in large, black, lower-case text on a white background. minimal and friendly",
+    "36": "a set is a collection of things, black and white, minimal, white background",
+    "72": "a collection of elephants, black and white, high contrast, minimal, white background, impression, unrealistic",
+    "96": "a collection of numbers, black and white, high contrast, minimal, white background, black text",
+    "120": "a deck of playing cards, black and white, high contrast, minimal, white background",
+    "156": "Members of a set, like the ace of spades, are called elements, black and white, high contrast, minimal, white background, impression",
+    "192": "mid century modern living room, black and white, high contrast, minimal, impression",
+    "216": "flat screen TV, black and white, high contrast, minimal, white background",
+    "240": "periodic table of elements. black and white, high contrast, minimal, white background, impression, black text",
+    "276": "two curly brackets. black and white, high contrast, minimal, white background, impression, black text",
+    "300": "Large capital letter A in a serif font, black and white, high contrast, minimal, white background, black text",
+    "324": "Large hand written capital letter A, black and white, high contrast, minimal, white background, black text"
+},
