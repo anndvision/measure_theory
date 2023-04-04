2985769f438799b4f3509d05d9c00266edf442f5
Andrew Jesson <56548831+anndvision@users.noreply.github.com>
2023-04-04 21:09:56 +0000

    add functions script and prompts

---
+++ episodes/02_functions/prompts.py
@@ -0,0 +1,13 @@
+animation_prompts = {
+    0: "book cover for 'Measure Theory Part 2, functions' in large, black, lower-case text on a white background. minimal and friendly",
+    60: "a set of modern clear empty glassware, white background, black and white image, minimal",
+    156: "modern stationery set, white background, black and white image, minimal",
+    228: "a single pencil standing atilt in a large modern clear glass, white background, black and white image, minimal",
+    348: "a set of modern clear empty glassware, white background, black and white image, minimal",
+    408: "modern stationery set, white background, black and white, minimal image",
+    468: "a single pencil standing atilt in a large modern clear glass and a stapler in a vase and pair of scissors in a small mason jar, white background, black and white image, minimal",
+    528: "a single simple stapler, white background, black and white image, minimal",
+    588: "a modern clear vase, white background, black and white image, minimal",
+    624: "a single simple stapler, white background, black and white image, minimal",
+    660: "a modern clear vase, white background, black and white image, minimal",
+}
---
+++ episodes/02_functions/script.md
@@ -0,0 +1,21 @@
+2.5@60      Measure Theory Part 2:
+
+            Functions
+
+3.0@72      A function establishes a relationship between two sets.
+
+3.0@72      For example, consider a set of glassware and a set of stationary.
+
+5.0@120     A function from the glassware to the stationery assigns a unique element of stationery to each glass in the glassware set.
+
+2.5@60      The set of glassware is called the function's domain, 
+
+2.5@60      and the set of stationery is called its co-domain.
+
+2.5@60      The function's range is the set of satationery that are actually assigned to glassware.
+
+4.5@108     The image of a function represents the the function's value evaluated at an element within the domain.
+
+5.0@120   Here, an pen would be the image of the function from glassware to stationery, evaluated at a specific vase.
+
+720
