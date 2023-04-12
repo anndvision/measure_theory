4a28a1e7de6aa370891c1c852f38035fb057a73b
Andrew Jesson <56548831+anndvision@users.noreply.github.com>
2023-04-12 09:04:47 +0000

    add episode 3 subsets

---
+++ episodes/03_subsets/prompts.py
@@ -0,0 +1,8 @@
+animation_prompts = {
+    0: "book cover for 'Measure Theory Part 3, subsets' in large, black, lower-case text on a white background. minimal and friendly",
+    60: "a deck of playing cards, grayscale image, high contrast, minimal, white background",
+    156: "mid century modern living room, grayscale image, high contrast, minimal, impression",
+    252: "a mid century modern sofa and a coffee table in an otherwise empty white room, grayscale image, high contrast, minimal, impression",
+    444: "mid century modern living room, grayscale image, high contrast, minimal, impression",
+    648: "nothingness, grayscale image, high contrast, minimal, impression",
+}
---
+++ episodes/03_subsets/script.md
@@ -0,0 +1,19 @@
+2.5@60      Measure Theory Part 3:
+
+            Subsets
+
+4@96        A subset is a concept that builds upon the idea of sets.
+
+5@120       For illustration, Imagine a set of living room furniture: a 
+		    coffee table, a lamp, a sofa…
+
+3@72        Now, consider just the sofa and the coffee table.
+
+4@96        This smaller set is a subset of the original set of 
+            furniture.
+
+5.5@132     In general, a set A is a subset of B if every element 
+            in A is also an element in B.
+
+6@144       Every set is a subset of itself, which            
+            will lead into the intriguing subject of Part 4: the empty set.
